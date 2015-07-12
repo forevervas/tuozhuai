a7e57d9259ad72f91c9dfdd80833af035139389c
xhq <1293812979@qq.com>
2015-07-12 21:17:29 +0800

    program
    program

---
+++ think_contents.sql
@@ -0,0 +1,45 @@
+-- phpMyAdmin SQL Dump
+-- version 3.3.8.1
+-- http://www.phpmyadmin.net
+--
+-- 主机: w.rdc.sae.sina.com.cn:3307
+-- 生成日期: 2015 年 07 月 12 日 19:35
+-- 服务器版本: 5.5.23
+-- PHP 版本: 5.3.3
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- 数据库: `app_thinkfortest`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `think_contents`
+--
+
+CREATE TABLE IF NOT EXISTS `think_contents` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `link_word` varchar(64) NOT NULL,
+  `link` varchar(128) DEFAULT NULL,
+  `sort` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 ;
+
+--
+-- 转存表中的数据 `think_contents`
+--
+
+INSERT INTO `think_contents` (`id`, `link_word`, `link`, `sort`) VALUES
+(49, '1', '1', 4),
+(50, '2', '2', 1),
+(51, '3', '3', 5),
+(52, '4', '4', 2),
+(53, '5', '5', 3);
